826fbaef6c22f394b901629e913e8c9fa0091a5b
charles2f <charles2f@yahoo.com.br>
2025-03-10 16:15:30 -0300

    Atualizando alguns tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception { // Método principal
         System.out.println("Hello, World!");
+
+        int idade = 10; // Variável inteira
+        idade = 17;
+        System.out.println(idade);  // Imprime a variável idade
+
+        double salario = 4560.0; // Variável double
+        salario = 6000.0;
+        System.out.println(salario); // Imprime a variável salario
+
+        final double PI = 3.14159; // Variável final
+        //PI = 3.14; // Erro - Não se pode alterar variavel final.
+        System.out.println(PI);  // Imprime a variável PI
+
     }
 }
